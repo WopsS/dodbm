c6a1df82853b15e7586c23d39e8e90a95c983aad
Octavian Dima <hello@wopss.ro>
2018-09-01 20:11:33 +0200

    Fix linux error code

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,7 +2,9 @@
 for f in $1/*-tests; do
     echo $(basename $f)
     $f
-    if [ $? -ne 0 ]; then
-        exit $?
+
+    error_code="$?"
+    if [ $error_code -ne 0 ]; then
+        exit $error_code
     fi
 done
